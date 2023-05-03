e5517879a2f5273ce5a3e03c1e36d5291c841c74
Sabinka@uran <ssamporova@gmail.com>
2023-05-03 16:45:29 +0200

    fix script for som color computing

--- pytorch/predict_train_som.py
+++ pytorch/predict_train_som.py
@@ -3,6 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 import main
 from mean_teacher import architectures, datasets, data, losses, ramps, cli
+from my_som import SOM
 
 
 class model_params:
@@ -26,7 +27,7 @@ class data_params:
 
 
 ds_name = 'cifar10'
-checkpoint_path = "results/main/2023-05-02_17:54:18/0/transient/best.ckpt"
+checkpoint_path = "results/best.ckpt"
 if torch.cuda.is_available():
     device = "cuda:0"
 else:
@@ -38,6 +39,7 @@ data_params = data_params()
 train_loader, eval_loader = main.create_data_loaders(**dataset_config, args=data_params)
 
 hyparams = model_params()
+hyparams.device = device
 model_factory = architectures.__dict__[hyparams.arch_name]
 model_params = dict(pretrained=False, num_classes=10)
 model = model_factory(**model_params)
@@ -53,20 +55,47 @@ model.load_state_dict(checkpoint['state_dict'])
 optimizer.load_state_dict(checkpoint['optimizer'])
 class_criterion = nn.CrossEntropyLoss(size_average=False).to(device)
 model.eval()
+
 loaded_data = []
+som = SOM(5, 5, 128, 10, hyparams).to(device)
+som.train()
+
+
+# SOM training
+for epoch in range(3):
+    for i, (input, target) in enumerate(eval_loader):
+        input_var = torch.autograd.Variable(input)
+        target_var = torch.autograd.Variable(target.to(device))
+        minibatch_size = len(target_var)
+        output1, output2, x_conv = model(input_var)
+        for x_c in x_conv:
+            with torch.no_grad():
+                som(x_c, epoch)
+            #print('train', torch.max(som.get_weights()))
+    print(epoch)
+   
+   
+# PREDICTION 
+d = {}
 for i, (input, target) in enumerate(eval_loader):
     input_var = torch.autograd.Variable(input)
     target_var = torch.autograd.Variable(target.to(device))
     minibatch_size = len(target_var)
     output1, output2, x_conv = model(input_var)
-    # loaded_data.append(x_conv)
-    # not working too much memory for the tensors
-    softmax1, softmax2 = F.softmax(output1, dim=1), F.softmax(output2, dim=1)
-    class_loss = class_criterion(output1, target_var) / minibatch_size
-    # measure accuracy and record loss
-    prec1, prec5 = main.accuracy(output1.data, target_var.data, topk=(1, 5))
-    print("Class {} Acc topk 1 {:.4f} 5 {:.4f}".format(target[0],prec1[0],prec5[0]))
-    print(x_conv.shape)
-    print(x_conv[0].shape)
-# print(loaded_data)
-# som = SOM(5, 5, 128, 10, hyparams).to(device)
+    for i,  x_c in enumerate(x_conv):
+        _, bmu_loc_1D = som.bmu_loc(x_c)
+        winner = som.weights[bmu_loc_1D]
+        
+
+        if bmu_loc_1D.item() not in d:
+            d[bmu_loc_1D.item()] = {}
+        if target_var[i].item() not in d[bmu_loc_1D.item()]:
+            d[bmu_loc_1D.item()][target_var[i].item()] = 0
+        #print(d)
+        #print(d[bmu_loc_1D])
+        #print(target_var)
+        #print(target_var[i])
+        #print(d[bmu_loc_1D][target_var[i]])
+        d[bmu_loc_1D.item()][target_var[i].item()] += 1
+
+print(d)
