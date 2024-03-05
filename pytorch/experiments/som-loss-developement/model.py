a2678ce1fd764974c34a13d66f5721315c4480af
sabinka@neptun <ssamporova@gmail.com>
2024-03-05 10:38:05 +0100

    add som implementation to separate file, pretrained som save and load when training model

--- pytorch/experiments/som-loss-developement/model.py
+++ pytorch/experiments/som-loss-developement/model.py
@@ -27,6 +27,7 @@ import matplotlib.pyplot as plt
 from math import *
 
 import time
+from som import SOM
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -39,7 +40,6 @@ wine = fetch_ucirepo(id=109)
 X = wine.data.features.to_numpy()  # (178, 13)
 y = wine.data.targets.to_numpy()  # 1, 2 or 3
 
-print(X.shape, y.shape)
 dataset_arr = np.hstack((X, y))
 batch_size = 30
 
@@ -60,7 +60,6 @@ class1 = class1[:48]  # 48x14
 class2 = class2[:48]
 class3 = class3[:48]
 
-print(class1.shape, class1[0])
 
 dataset = []
 paired_labels = []
@@ -84,7 +83,6 @@ for i in range(size):
         dataset.append((class2[i], class3[j]))
         paired_labels.append(6)
 
-print(len(dataset))  # 13680 list of tuples of ndarrays of data and label
 
 paired_train_dataset, paired_test_dataset, labels1, labels2 = sklearn.model_selection.train_test_split(dataset,
                                                                                                        paired_labels,
@@ -92,16 +90,13 @@ paired_train_dataset, paired_test_dataset, labels1, labels2 = sklearn.model_sele
                                                                                                        random_state=42,
                                                                                                        shuffle=True,
                                                                                                        stratify=paired_labels)
-print(len(paired_train_dataset), len(paired_test_dataset))
 
-print("xx", np.array(paired_train_dataset).shape)
 train_dataloader = DataLoader(torch.tensor(np.array(paired_train_dataset)).to(device), batch_size=batch_size,
                               shuffle=True)
 test_dataloader = DataLoader(torch.tensor(np.array(paired_test_dataset)).to(device), batch_size=batch_size,
                              shuffle=True)
 
 som_dataloader = DataLoader(torch.tensor(np.concatenate((class1, class2, class3))).to(device), shuffle=True)
-print(len(som_dataloader))
 
 
 def generate_triplet(x, class1, class2):
@@ -160,331 +155,12 @@ print(dataset_triplets.shape)
 train_dataloader = DataLoader(torch.tensor(dataset_triplets).to(device), batch_size=batch_size, shuffle=True)
 
 
-### UTILS + VISUALIZATION
-
+### UTILS
 def one_hot(labels, classes, offset):
     labels = labels - offset
     labels = labels.squeeze().to(torch.int)
     res = torch.eye(classes, device=device)[labels.long()]
     return res.to(device)
-
-
-# print(one_hot(to)rch.tensor([1, 2, 3, 1, 3]), 3, 1))
-
-def show_umatrix(n, m, d, offset=0, fig_file=""):
-    neuron_classes = []
-    percentage = []
-    x = []
-    y = []
-    for r in range(n):
-        for c in range(m):
-
-            act_neuron = r * n + c
-            if act_neuron not in d:
-                continue
-
-            neuron_classes.append(max(d[act_neuron], key=d[act_neuron].get) + offset)
-            percentage.append(d[act_neuron][neuron_classes[-1] - offset] / sum(d[act_neuron].values()) * 100 * 2.5)
-            x.append(c)
-            y.append(r)
-
-    plt.figure()
-    c = neuron_classes
-    s = percentage
-    plt.rc('axes', axisbelow=True)
-    plt.xticks(np.arange(-2, n, 1))
-    plt.xlim(-2, n)
-    plt.yticks(np.arange(-1, m, 1))
-    plt.ylim(-1, m)
-    plt.grid(linestyle='dashed')
-    scatter = plt.scatter(x, y, c=c, s=s, cmap='turbo')
-    plt.legend(*scatter.legend_elements(),
-               loc="lower left", title="Classes")
-    # plt.imshow(neuron_classes, cmap='viridis', interpolation='nearest')
-    # plt.colorbar()
-    plt.title(f'Neuron activation classes {n}x{m}')
-    plt.show()
-
-    if fig_file != "":
-        plt.savefig(fig_file)
-
-
-def show_som_stats(all_quant, all_winner, all_entr, all_dist=[]):
-    plt.figure(figsize=(12, 4))
-    plt.subplot(141)  # 1 row, 3 columns, 1st subplot
-    plt.plot(torch.tensor(all_quant).cpu())
-    plt.title('quant_err')
-
-    plt.subplot(142)  # 1 row, 3 columns, 2nd subplot
-    plt.plot(all_winner)
-    plt.title('winner_discrimination')
-
-    plt.subplot(143)  # 1 row, 3 columns, 3rd subplot
-    plt.plot(all_entr)
-    plt.title('entropy')
-
-    plt.subplot(144)
-    plt.plot(all_dist)
-    plt.title('sample - prototype distance')
-
-    plt.tight_layout()
-
-    plt.show()
-
-
-def show_conf_matrix(confusion, class_labels):
-    plt.figure(figsize=(4, 4))
-    sns.set(font_scale=1.2)  # Adjust the font size for clarity
-
-    sns.heatmap(confusion, annot=True, fmt="d", cmap="Blues",
-                xticklabels=class_labels, yticklabels=class_labels)
-
-    plt.xlabel("Predicted")
-    plt.ylabel("Actual")
-    plt.title("Confusion Matrix")
-    plt.show()
-
-
-class Losses4:
-    def __init__(self, total_loss, sup_loss, som_loss_same, som_loss_dif) -> None:
-        self.total_loss = total_loss
-        self.sup_loss = sup_loss
-        self.som_loss_same = som_loss_same
-        self.som_loss_dif = som_loss_dif
-
-
-def show_3losses(losses):
-    plt.figure(figsize=(16, 4))
-
-    plt.subplot(141)  # 1 row, 3 columns, 1st subplot
-    plt.plot(losses.total_loss)
-    plt.title('total loss')
-
-    plt.subplot(142)  # 1 row, 3 columns, 2nd subplot
-    plt.plot(losses.sup_loss)
-    plt.title('supervised loss')
-
-    plt.subplot(143)  # 1 row, 3 columns, 3rd subplot
-    plt.plot(losses.som_loss_same)
-    plt.title('som congruent dist')
-
-    plt.subplot(144)  # 1 row, 3 columns, 3rd subplot
-    plt.plot(losses.som_loss_dif)
-    plt.title('som incongruent dist')
-
-    plt.tight_layout()
-
-    plt.show()
-
-
-### SOM
-
-class SOM(nn.Module):
-    """
-    2-D Self-Organizing Map with Gaussian Neighbourhood function
-    and linearly decreasing learning rate.
-    """
-
-    def __init__(self, m, n, dim, niter, args, alpha=None, sigma=None):
-        super(SOM, self).__init__()
-        self.args = args
-        self.m = m
-        self.n = n
-        self.dim = dim
-        self.niter = niter
-        if alpha is None:
-            self.alpha = 0.3
-        else:
-            self.alpha = float(alpha)
-        if sigma is None:
-            self.sigma = max(m, n) / 2.0
-        else:
-            self.sigma = float(sigma)
-
-        self.weights = torch.randn(m * n, dim).to(device)
-        self.locations = torch.LongTensor(np.array(list(self.neuron_locations())))
-        self.pdist = nn.PairwiseDistance(p=2)
-        self.quant_err = 0
-        self.num_err = 0
-        self.winner_occurences = [0 for i in range(self.m * self.n)]
-        self.d = {}
-        self.dist_sum = 0
-
-        self.all_quant_err = []
-        self.all_winner = []
-        self.all_entr = []
-        self.all_dists = []
-
-    def get_weights(self):
-        return self.weights
-
-    def get_locations(self):
-        return self.locations
-
-    def get_som_stats(self):
-        quant_err = self.quant_err / self.num_err
-        winner_discrimination = sum(tmp > 0 for tmp in self.winner_occurences) / (self.m * self.n)
-
-        px = torch.FloatTensor([x / self.num_err for x in self.winner_occurences])
-        logpx = torch.log2(px)
-        product = px * logpx
-        entropy = - torch.nansum(product)
-
-        dists = self.dist_sum / self.num_err
-
-        if isnan(entropy):
-            print("self.winner_occurences {} \npx {} \nlogpx {} \nproduct {}".format(self.winner_occurences, px, logpx,
-                                                                                     product))
-
-        self.quant_err = 0
-        self.num_err = 0
-        self.winner_occurences = [0 for i in range(self.m * self.n)]
-        self.dist_sum = 0
-
-        return quant_err, winner_discrimination, entropy, dists
-
-    def save_som_stats(self):
-        quant_err, winner_discrimination, entropy, dists = self.get_som_stats()
-
-        self.all_quant_err.append(quant_err)
-        self.all_winner.append(winner_discrimination)
-        self.all_entr.append(entropy)
-        self.all_dists.append(dists.item())
-
-        return quant_err, winner_discrimination, entropy, dists
-
-    def neuron_locations(self):
-        for i in range(self.m):
-            for j in range(self.n):
-                yield np.array([i, j])
-
-    def map_vects(self, input_vects):
-        to_return = []
-        for vect in input_vects:
-            min_index = min([i for i in range(len(self.weights))],
-                            key=lambda x: torch.norm(vect - self.weights[x]))
-            to_return.append(self.locations[min_index])
-
-        return to_return
-
-    def bmu_loc(self, x):
-
-        x_matrix = torch.stack([x.squeeze() for i in range(self.m * self.n)]).to(device)
-        dists = self.pdist(x_matrix, self.weights)
-        _, bmu_index = torch.min(dists, 0)
-        bmu_loc = self.locations[bmu_index, :]
-        bmu_loc = bmu_loc.squeeze()
-
-        return bmu_loc, bmu_loc[0] * self.m + bmu_loc[1]
-
-    def forward(self, x, y, it):
-
-        """ Take one input x and find location of its BMU in 2D net,
-            then calculate distances of all neurons to this BMU and
-            update their positions. """
-
-        bmu_loc, bmu_loc_1D = self.bmu_loc(x)
-
-        # calculate distance of bmu position in ND space and x
-        winner = self.weights[bmu_loc_1D]  # winner position in 3d space
-
-        if it == -1:  # test, no som weight adjustment
-            return winner
-
-        self.quant_err += torch.sum(torch.pow(x - winner, 2))
-        self.num_err += 1
-        self.dist_sum += torch.norm(x.squeeze() - winner)
-        # print(x, winner, torch.norm(x-winner), sep="\n")
-
-        # calculate winner occurences for stats
-        self.winner_occurences[bmu_loc_1D] += 1
-
-        if bmu_loc_1D.item() not in self.d:
-            self.d[bmu_loc_1D.item()] = {}
-        if y.item() not in self.d[bmu_loc_1D.item()]:
-            self.d[bmu_loc_1D.item()][y.item()] = 0
-        self.d[bmu_loc_1D.item()][y.item()] += 1
-
-        learning_rate_op = 1.0 - it / self.niter
-        alpha_op = self.alpha * learning_rate_op
-        sigma_op = self.sigma * learning_rate_op
-
-        tmp = torch.stack([bmu_loc for i in range(self.m * self.n)])
-        tmp = self.locations.float() - tmp.float()
-        tmp = torch.pow(tmp, 2)
-        bmu_distance_squares = torch.sum(tmp, 1)
-
-        neighbourhood_func = torch.exp(torch.neg(torch.div(bmu_distance_squares, sigma_op ** 2)))
-
-        learning_rate_op = alpha_op * neighbourhood_func
-
-        learning_rate_multiplier = torch.stack(
-            [learning_rate_op[i:i + 1].repeat(self.dim) for i in range(self.m * self.n)])
-        delta = torch.mul(learning_rate_multiplier.to(device),
-                          (torch.stack([x.squeeze() for i in range(self.m * self.n)]).to(device) - self.weights))
-
-        # print("self.weights: ", self.weights.shape, "delta: ", delta.shape)
-        new_weights = torch.add(self.weights, delta)
-        self.weights = new_weights
-        # print("self.weights2: ", self.weights.shape)
-
-        return winner
-
-    def predict(self, x):
-
-        bmu_loc, bmu_loc_1D = self.bmu_loc(x)
-        # winner = self.weights[bmu_loc_1D]
-        winner = bmu_loc
-        return winner
-
-
-repeat = True
-EPS = 20
-som_size = 6
-
-while repeat:
-    som = SOM(som_size, som_size, 13, EPS, {}).to(device)
-    som.train()
-
-    all_quant = []
-    all_winner = []
-    all_entr = []
-
-    ds = []
-
-    for ep in range(EPS):
-        som.d = {}
-        print(f"Epoch: {ep + 1}", end=" : ")
-        with torch.no_grad():
-            for batch, sample1 in enumerate(som_dataloader):
-                Xs1, ys1 = sample1[:, :-1].type(torch.float32).to(device), sample1[:, -1:].type(torch.float32).to(
-                    device)
-
-                som(Xs1, ys1, ep)
-
-        cur_quant_err, cur_winner_discrimination, cur_entropy, dists = som.save_som_stats()
-
-        print(
-            f"SOM trained on new x_convs, quant_err: {cur_quant_err}, winner_discrimination: {cur_winner_discrimination}, entropy: {cur_entropy}, SP dist: {dists}",
-            sep="\t")
-
-        # if ep % 30 == 0:
-        # show_umatrix(5, 5,som.d)
-        # show_som_stats(som.all_quant_err, som.all_winner, som.all_entr, som.all_dists)
-
-        ds.append(som.d)
-
-    show_umatrix(som_size, som_size, som.d, 0, "som.png")
-
-    print("Are you okay with SOM ? Y/N")
-    if input().strip() == "Y":
-        repeat = False
-
-# show_som_stats(som.all_quant_err, som.all_winner, som.all_entr, som.all_dists)
-
-som_stats = {"qe": [round(tensor.item(), 5) for tensor in som.all_quant_err],
-             "wd": [round(i, 5) for i in som.all_winner], "e": [round(tensor.item(), 5) for tensor in som.all_entr],
-             "dist": [round(i, 5) for i in som.all_dists]}
 
 
 ### MODEL
@@ -623,11 +299,14 @@ loss_fn = SomSupLoss()
 loss_fn2 = nn.MSELoss()
 optimizer = torch.optim.Adam(mlp.parameters(), lr=0.0001)
 
-EPS = 50
-MODS = 10
+som = torch.load("pretrained_som1709630916.2606385.pt")
+som.eval()
+
+EPS = 100
+MODS = 20
 final_losses, accs, confs = {}, {}, {}
 tm = time.time()
-for kappa in [0.5, 0.2, 0.8, 0.1, 0.01, 0.001]:
+for kappa in [0, 0.7, 0.8]:
 	for mod_i in range(10):
 		print(f"{mod_i + 1}. model starts in {tm - time.time()} sec")
 		mlp = NeuralNetwork().to(device)
---
+++ pytorch/experiments/som-loss-developement/som.py
@@ -0,0 +1,426 @@
+import subprocess
+import sys
+
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+install("ucimlrepo")  # install wine dataset repo
+install("seaborn")
+from ucimlrepo import fetch_ucirepo
+
+
+import numpy as np
+import json
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.nn.modules.loss import MSELoss
+import torch.nn.functional as F
+
+import sklearn.model_selection
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+from math import *
+import time
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+### DOWNLOAD DATA
+
+# fetch dataset
+wine = fetch_ucirepo(id=109)
+
+# data (as pandas dataframes)
+X = wine.data.features.to_numpy()  # (178, 13)
+y = wine.data.targets.to_numpy()  # 1, 2 or 3
+
+dataset_arr = np.hstack((X, y))
+batch_size = 30
+
+### PREPARE DATA
+
+# divide into classes
+class1 = dataset_arr[:59]  # 59x14
+class2 = dataset_arr[59:130]
+class3 = dataset_arr[130:]
+
+# TODO nevadi ze to je nahodne a nie pseudonahodne ??
+np.random.shuffle(class1)  # 59x14
+np.random.shuffle(class2)
+np.random.shuffle(class3)
+
+size = 48
+class1 = class1[:48]  # 48x14
+class2 = class2[:48]
+class3 = class3[:48]
+
+
+dataset = []
+paired_labels = []
+
+for i in range(size):
+    for j in range(size):
+        if i != j:
+            dataset.append((class1[i], class1[j]))
+            paired_labels.append(1)
+            dataset.append((class2[i], class2[j]))
+            paired_labels.append(2)
+            dataset.append((class3[i], class3[j]))
+            paired_labels.append(3)
+
+for i in range(size):
+    for j in range(size):
+        dataset.append((class1[i], class2[j]))
+        paired_labels.append(4)
+        dataset.append((class1[i], class3[j]))
+        paired_labels.append(5)
+        dataset.append((class2[i], class3[j]))
+        paired_labels.append(6)
+
+
+paired_train_dataset, paired_test_dataset, labels1, labels2 = sklearn.model_selection.train_test_split(dataset,
+                                                                                                       paired_labels,
+                                                                                                       test_size=0.25,
+                                                                                                       random_state=42,
+                                                                                                       shuffle=True,
+                                                                                                       stratify=paired_labels)
+
+train_dataloader = DataLoader(torch.tensor(np.array(paired_train_dataset)).to(device), batch_size=batch_size,
+                              shuffle=True)
+test_dataloader = DataLoader(torch.tensor(np.array(paired_test_dataset)).to(device), batch_size=batch_size,
+                             shuffle=True)
+
+som_dataloader = DataLoader(torch.tensor(np.concatenate((class1, class2, class3))).to(device), shuffle=True)
+
+### UTILS
+
+def show_umatrix(n, m, d, offset=0, fig_file=""):
+    neuron_classes = []
+    percentage = []
+    x = []
+    y = []
+    for r in range(n):
+        for c in range(m):
+
+            act_neuron = r * n + c
+            if act_neuron not in d:
+                continue
+
+            neuron_classes.append(max(d[act_neuron], key=d[act_neuron].get) + offset)
+            percentage.append(d[act_neuron][neuron_classes[-1] - offset] / sum(d[act_neuron].values()) * 100 * 2.5)
+            x.append(c)
+            y.append(r)
+
+    plt.figure()
+    c = neuron_classes
+    s = percentage
+    plt.rc('axes', axisbelow=True)
+    plt.xticks(np.arange(-2, n, 1))
+    plt.xlim(-2, n)
+    plt.yticks(np.arange(-1, m, 1))
+    plt.ylim(-1, m)
+    plt.grid(linestyle='dashed')
+    scatter = plt.scatter(x, y, c=c, s=s, cmap='turbo')
+    plt.legend(*scatter.legend_elements(),
+               loc="lower left", title="Classes")
+    # plt.imshow(neuron_classes, cmap='viridis', interpolation='nearest')
+    # plt.colorbar()
+    plt.title(f'Neuron activation classes {n}x{m}')
+    plt.show()
+
+    if fig_file != "":
+        plt.savefig(fig_file)
+
+
+def show_som_stats(all_quant, all_winner, all_entr, all_dist=[]):
+    plt.figure(figsize=(12, 4))
+    plt.subplot(141)  # 1 row, 3 columns, 1st subplot
+    plt.plot(torch.tensor(all_quant).cpu())
+    plt.title('quant_err')
+
+    plt.subplot(142)  # 1 row, 3 columns, 2nd subplot
+    plt.plot(all_winner)
+    plt.title('winner_discrimination')
+
+    plt.subplot(143)  # 1 row, 3 columns, 3rd subplot
+    plt.plot(all_entr)
+    plt.title('entropy')
+
+    plt.subplot(144)
+    plt.plot(all_dist)
+    plt.title('sample - prototype distance')
+
+    plt.tight_layout()
+
+    plt.show()
+
+
+def show_conf_matrix(confusion, class_labels):
+    plt.figure(figsize=(4, 4))
+    sns.set(font_scale=1.2)  # Adjust the font size for clarity
+
+    sns.heatmap(confusion, annot=True, fmt="d", cmap="Blues",
+                xticklabels=class_labels, yticklabels=class_labels)
+
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.title("Confusion Matrix")
+    plt.show()
+
+
+class Losses4:
+    def __init__(self, total_loss, sup_loss, som_loss_same, som_loss_dif) -> None:
+        self.total_loss = total_loss
+        self.sup_loss = sup_loss
+        self.som_loss_same = som_loss_same
+        self.som_loss_dif = som_loss_dif
+
+
+def show_3losses(losses):
+    plt.figure(figsize=(16, 4))
+
+    plt.subplot(141)  # 1 row, 3 columns, 1st subplot
+    plt.plot(losses.total_loss)
+    plt.title('total loss')
+
+    plt.subplot(142)  # 1 row, 3 columns, 2nd subplot
+    plt.plot(losses.sup_loss)
+    plt.title('supervised loss')
+
+    plt.subplot(143)  # 1 row, 3 columns, 3rd subplot
+    plt.plot(losses.som_loss_same)
+    plt.title('som congruent dist')
+
+    plt.subplot(144)  # 1 row, 3 columns, 3rd subplot
+    plt.plot(losses.som_loss_dif)
+    plt.title('som incongruent dist')
+
+    plt.tight_layout()
+
+    plt.show()
+
+
+
+
+### SOM
+
+class SOM(nn.Module):
+    """
+    2-D Self-Organizing Map with Gaussian Neighbourhood function
+    and linearly decreasing learning rate.
+    """
+
+    def __init__(self, m, n, dim, niter, args, alpha=None, sigma=None):
+        super(SOM, self).__init__()
+        self.args = args
+        self.m = m
+        self.n = n
+        self.dim = dim
+        self.niter = niter
+        if alpha is None:
+            self.alpha = 0.3
+        else:
+            self.alpha = float(alpha)
+        if sigma is None:
+            self.sigma = max(m, n) / 2.0
+        else:
+            self.sigma = float(sigma)
+
+        self.weights = torch.randn(m * n, dim).to(device)
+        self.locations = torch.LongTensor(np.array(list(self.neuron_locations())))
+        self.pdist = nn.PairwiseDistance(p=2)
+        self.quant_err = 0
+        self.num_err = 0
+        self.winner_occurences = [0 for i in range(self.m * self.n)]
+        self.d = {}
+        self.dist_sum = 0
+
+        self.all_quant_err = []
+        self.all_winner = []
+        self.all_entr = []
+        self.all_dists = []
+
+    def get_weights(self):
+        return self.weights
+
+    def get_locations(self):
+        return self.locations
+
+    def get_som_stats(self):
+        quant_err = self.quant_err / self.num_err
+        winner_discrimination = sum(tmp > 0 for tmp in self.winner_occurences) / (self.m * self.n)
+
+        px = torch.FloatTensor([x / self.num_err for x in self.winner_occurences])
+        logpx = torch.log2(px)
+        product = px * logpx
+        entropy = - torch.nansum(product)
+
+        dists = self.dist_sum / self.num_err
+
+        if isnan(entropy):
+            print("self.winner_occurences {} \npx {} \nlogpx {} \nproduct {}".format(self.winner_occurences, px, logpx,
+                                                                                     product))
+
+        self.quant_err = 0
+        self.num_err = 0
+        self.winner_occurences = [0 for i in range(self.m * self.n)]
+        self.dist_sum = 0
+
+        return quant_err, winner_discrimination, entropy, dists
+
+    def save_som_stats(self):
+        quant_err, winner_discrimination, entropy, dists = self.get_som_stats()
+
+        self.all_quant_err.append(quant_err)
+        self.all_winner.append(winner_discrimination)
+        self.all_entr.append(entropy)
+        self.all_dists.append(dists.item())
+
+        return quant_err, winner_discrimination, entropy, dists
+
+    def neuron_locations(self):
+        for i in range(self.m):
+            for j in range(self.n):
+                yield np.array([i, j])
+
+    def map_vects(self, input_vects):
+        to_return = []
+        for vect in input_vects:
+            min_index = min([i for i in range(len(self.weights))],
+                            key=lambda x: torch.norm(vect - self.weights[x]))
+            to_return.append(self.locations[min_index])
+
+        return to_return
+
+    def bmu_loc(self, x):
+
+        x_matrix = torch.stack([x.squeeze() for i in range(self.m * self.n)]).to(device)
+        dists = self.pdist(x_matrix, self.weights)
+        _, bmu_index = torch.min(dists, 0)
+        bmu_loc = self.locations[bmu_index, :]
+        bmu_loc = bmu_loc.squeeze()
+
+        return bmu_loc, bmu_loc[0] * self.m + bmu_loc[1]
+
+    def forward(self, x, y, it):
+
+        """ Take one input x and find location of its BMU in 2D net,
+            then calculate distances of all neurons to this BMU and
+            update their positions. """
+
+        bmu_loc, bmu_loc_1D = self.bmu_loc(x)
+
+        # calculate distance of bmu position in ND space and x
+        winner = self.weights[bmu_loc_1D]  # winner position in 3d space
+
+        if it == -1:  # test, no som weight adjustment
+            return winner
+
+        self.quant_err += torch.sum(torch.pow(x - winner, 2))
+        self.num_err += 1
+        self.dist_sum += torch.norm(x.squeeze() - winner)
+        # print(x, winner, torch.norm(x-winner), sep="\n")
+
+        # calculate winner occurences for stats
+        self.winner_occurences[bmu_loc_1D] += 1
+
+        if bmu_loc_1D.item() not in self.d:
+            self.d[bmu_loc_1D.item()] = {}
+        if y.item() not in self.d[bmu_loc_1D.item()]:
+            self.d[bmu_loc_1D.item()][y.item()] = 0
+        self.d[bmu_loc_1D.item()][y.item()] += 1
+
+        learning_rate_op = 1.0 - it / self.niter
+        alpha_op = self.alpha * learning_rate_op
+        sigma_op = self.sigma * learning_rate_op
+
+        tmp = torch.stack([bmu_loc for i in range(self.m * self.n)])
+        tmp = self.locations.float() - tmp.float()
+        tmp = torch.pow(tmp, 2)
+        bmu_distance_squares = torch.sum(tmp, 1)
+
+        neighbourhood_func = torch.exp(torch.neg(torch.div(bmu_distance_squares, sigma_op ** 2)))
+
+        learning_rate_op = alpha_op * neighbourhood_func
+
+        learning_rate_multiplier = torch.stack(
+            [learning_rate_op[i:i + 1].repeat(self.dim) for i in range(self.m * self.n)])
+        delta = torch.mul(learning_rate_multiplier.to(device),
+                          (torch.stack([x.squeeze() for i in range(self.m * self.n)]).to(device) - self.weights))
+
+        # print("self.weights: ", self.weights.shape, "delta: ", delta.shape)
+        new_weights = torch.add(self.weights, delta)
+        self.weights = new_weights
+        # print("self.weights2: ", self.weights.shape)
+
+        return winner
+
+    def predict(self, x):
+
+        bmu_loc, bmu_loc_1D = self.bmu_loc(x)
+        # winner = self.weights[bmu_loc_1D]
+        winner = bmu_loc
+        return winner
+
+
+def pretrain_som():
+
+	repeat = True
+	EPS = 20
+	som_size = 6
+	exp_run_time = time.time()
+
+	while repeat:
+		som = SOM(som_size, som_size, 13, EPS, {}).to(device)
+		som.train()
+
+		all_quant = []
+		all_winner = []
+		all_entr = []
+
+		ds = []
+
+		for ep in range(EPS):
+		    som.d = {}
+		    print(f"Epoch: {ep + 1}", end=" : ")
+		    with torch.no_grad():
+		        for batch, sample1 in enumerate(som_dataloader):
+		            Xs1, ys1 = sample1[:, :-1].type(torch.float32).to(device), sample1[:, -1:].type(torch.float32).to(
+		                device)
+
+		            som(Xs1, ys1, ep)
+
+		    cur_quant_err, cur_winner_discrimination, cur_entropy, dists = som.save_som_stats()
+
+		    print(
+		        f"SOM trained on new x_convs, quant_err: {cur_quant_err}, winner_discrimination: {cur_winner_discrimination}, entropy: {cur_entropy}, SP dist: {dists}",
+		        sep="\t")
+
+		    # if ep % 30 == 0:
+		    # show_umatrix(5, 5,som.d)
+		    # show_som_stats(som.all_quant_err, som.all_winner, som.all_entr, som.all_dists)
+
+		    ds.append(som.d)
+
+		show_umatrix(som_size, som_size, som.d, 0, f"som{exp_run_time}.png")
+		torch.save(som, f"pretrained_som{exp_run_time}.pt")
+
+
+		if cur_winner_discrimination == 1.0:
+			print("Are you okay with SOM ? Y/N")
+			if input().strip() == "Y":
+				repeat = False
+
+
+	# show_som_stats(som.all_quant_err, som.all_winner, som.all_entr, som.all_dists)
+
+	som_stats = {"qe": [round(tensor.item(), 5) for tensor in som.all_quant_err],
+		         "wd": [round(i, 5) for i in som.all_winner], "e": [round(tensor.item(), 5) for tensor in som.all_entr],
+		         "dist": [round(i, 5) for i in som.all_dists]}
+
+
